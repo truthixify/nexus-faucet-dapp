decd054117a28b51758742987a02e9d28708a756
truthixify <truthixify@gmail.com>
2025-02-22 03:22:53 +0100

    next lint again

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useCallback, useRef } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { BrowserProvider, JsonRpcSigner, Contract, parseEther } from 'ethers'
 import abi from "../../public/abi.json"
 
